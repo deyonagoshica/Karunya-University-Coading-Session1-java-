cb9feab56af14638770db73a83fadfb3875904ca
Deyona goshica <154041023+deyonagoshica@users.noreply.github.com>
2025-09-09 16:15:21 +0530

    Create check whether the time is valid or not using conditional construct..java

---
+++ check whether the time is valid or not using conditional construct..java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String time = sc.nextLine();
+        String[] parts = time.split(":");
+        if (parts.length == 3) {
+            int hh = Integer.parseInt(parts[0]);
+            int mm = Integer.parseInt(parts[1]);
+            int ss = Integer.parseInt(parts[2]);
+
+            if (hh >= 0 && hh < 24 && mm >= 0 && mm < 60 && ss >= 0 && ss < 60) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Not Valid");
+            }
+        } else {
+            System.out.println("Not Valid");
+        }
+    }
+}
