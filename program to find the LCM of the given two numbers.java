b40ca1aad2f03f1d5af694566f93c67f316d9ef9
Deyona goshica <154041023+deyonagoshica@users.noreply.github.com>
2025-09-10 14:53:41 +0530

    Create program to find the LCM of the given two numbers.java

---
+++ program to find the LCM of the given two numbers.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        int gcd = 1;
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+        int lcm = (a * b) / gcd;
+        System.out.println(lcm);
+    }
+}
