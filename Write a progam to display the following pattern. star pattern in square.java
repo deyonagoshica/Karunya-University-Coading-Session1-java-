fdc61ead04e001ad16fedcba55d65bd8657ccfc5
Deyona goshica <154041023+deyonagoshica@users.noreply.github.com>
2025-09-11 14:25:26 +0530

    Create Write a progam to display the following pattern. star pattern in square.java

---
+++ Write a progam to display the following pattern. star pattern in square.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n, rows, colm;
+        n=sc.nextInt();
+        for(rows=1;rows<=n;rows++){
+            for(colm=1;colm<=n;colm++){
+                System.out.print("*");                                 
+            }
+        System.out.println();
+        }
+    }
+}
