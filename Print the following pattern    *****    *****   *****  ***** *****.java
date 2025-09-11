47d7136afc0b9f7827a61cbd1d35e9303b388e22
Deyona goshica <154041023+deyonagoshica@users.noreply.github.com>
2025-09-11 14:40:20 +0530

    Create Print the following pattern    *****    *****   *****  ***** *****.java

---
+++ Print the following pattern    *****    *****   *****  ***** *****.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int row = 1; row <= n; row++) {
+            for (int space = 1; space <= n - row; space++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= n; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
