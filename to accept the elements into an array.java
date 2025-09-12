8fd6ea18202227af5abd1804a9a143051e04b6d2
Deyona goshica <154041023+deyonagoshica@users.noreply.github.com>
2025-09-12 15:08:19 +0530

    Create to accept the elements into an array.java

---
+++ to accept the elements into an array.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] result = new int[n];
+        int sum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = sum;
+            sum += arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
+}
